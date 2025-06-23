425c025bf9d17dfae8815daef83625982c656bda
Thomas Candille <114737865+ThomasCandille@users.noreply.github.com>
2025-06-23 12:15:37 +0200

    feat: enhance keypad functionality and styling with new assets

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,19 +3,44 @@ import './App.css'
 
 function App() {
 
+const keypadDisplay = document.querySelector('.keypad-display');
+const keypadContainer = document.querySelector('.keypad-display-container') as HTMLElement
+
   return (
     <div className='App'>
+      <div className='keypad-container'>
       <div className='keypad-display-container'>
         <p className='keypad-display' />
       </div>
       <Keypad
         onNumberClick={(number) => {
-          const element = document.querySelector('.keypad-display');
-          if (element) {
-            element.textContent += number.toString();
+          if (keypadDisplay) {
+
+            if (keypadDisplay.textContent?.length === 4 && typeof number === 'number') {
+              return;
+            }
+
+            if (keypadDisplay.textContent?.length === 0 && typeof number === 'number') {
+              keypadContainer.style.backgroundColor = '#34C0AE';
+            }
+
+            if (number === '✕') {
+              keypadDisplay.textContent = keypadDisplay.textContent?.slice(0, -1) || '';
+            }
+
+            else if (number === '✓') {
+              const code = keypadDisplay.textContent;
+              keypadContainer.style.backgroundColor = code === '0000' ? '#54B95C' : '#B03636';
+              keypadDisplay.textContent = '';
+            }
+
+            else{
+              keypadDisplay.textContent += number.toString();
+            }
           }
         }}
       />
+      </div>
     </div>
   )
 }
--- front/src/components/keypadNumber/KeypadNumber.tsx
+++ front/src/components/keypadNumber/KeypadNumber.tsx
@@ -1,8 +1,8 @@
 import './KeypadNumber.css';
 
 interface KeypadNumberProps {
-    number: number;
-    onClick: (number: number) => void;
+    number: number|string;
+    onClick: (number: number|string) => void;
 }
 
 export function KeypadNumber({ number, onClick }: KeypadNumberProps) {
